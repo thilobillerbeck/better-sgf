b46a659e17be8bdacc9c7870630c18bf4b2d71ca
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2022-05-28 17:08:43 +0200

    updated metadata

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,13 +9,31 @@ export default defineConfig({
     react(),
     Icons({ compiler: "jsx", jsx: "react" }),
     VitePWA({
-      includeAssets: ["src/favicon.svg"],
+      mode: "development",
+      base: "/",
+      includeAssets: ["favicon.svg"],
       manifest: {
         name: "BetterSGF",
         short_name: "BetterSGF",
         description:
           "Eine inoffizielle Companion App für das Schlossgrabenfest",
-        theme_color: "#ffffff",
+        theme_color: "#f69435",
+        background_color: "#f69435",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "android-chrome-192x192.png", // <== don't add slash, for testing
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png", // <== don't remove slash, for testing
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
       devOptions: {
         enabled: true,
